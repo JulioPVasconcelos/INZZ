9f8077ae3d9f1ca8d6fb37e1192f75ef0c5649a1
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-09-04 21:29:38 -0300

    Create ideias_de_telas.c
    alguns caracteres não funcionam e bugam o programa inteiro mesmo estando comentado

---
+++ ideias_de_telas.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+
+int main(){
+    setlocale(LC_ALL, "");
+
+    //printf("╔════════════════════════════════════╗");
+    //printf("║                                    ║");
+    //printf("║                                    ║");
+    //printf("║                                    ║");
+    //printf("║                                    ║");
+    //printf("║                                    ║");
+    //printf("║                                    ║");
+    //printf("║                                    ║");
+    //printf("║                                    ║");
+    //printf("║                                    ║");
+    //printf("║                                    ║");
+    //printf("╚════════════════════════════════════╝");
+
+    printf("|------------------------------------|\n");
+    printf("|                                    |\n");
+    printf("|                                    |\n");
+    printf("|                                    |\n");
+    printf("|               Olá                  |\n");
+    printf("|                                    |\n");
+    printf("|                                    |\n");
+    printf("|                                    |\n");
+    printf("|                                    |\n");
+    printf("|                                    |\n");
+    printf("|                                    |\n");
+    printf("|------------------------------------|\n\n");
+
+    printf(" ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+    printf("<                                     >\n");
+    printf("<                                     >\n");
+    printf("<                                     >\n");
+    printf("<               Olá                   >\n");
+    printf("<                                     >\n");
+    printf("<                                     >\n");
+    printf("<                                     >\n");
+    printf("<                                     >\n");
+    printf("<                                     >\n");
+    printf("<                                     >\n");
+    printf(" vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n\n");
+
+    printf("Caracteres ASCII comuns:\n");
+    printf("Aspas Duplas: \"\n");
+    printf("Aspas Simples: '\n");
+    printf("Barra Invertida: \\ \n");
+    printf("Parênteses: () \n");
+    printf("Colchetes: [] \n");
+    printf("Chaves: {} \n");
+    printf("Linha Horizontal: -\n");
+    printf("Linha Vertical: |\n");
+
+    // Caracteres Unicode
+    printf("\nCaracteres Unicode:\n");
+    printf("Coração: ♥\n");
+    printf("Estrela: ★\n");
+    printf("Símbolo de Euro: €\n");
+    printf("Símbolo de Yen: ¥\n");
+}
